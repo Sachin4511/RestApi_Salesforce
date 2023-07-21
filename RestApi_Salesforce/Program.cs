2df3720fb8bcc3f31f5608b03ed44f7af3d76893
Sachin V Bajantri <sachin-v.bajantri@capgemini.com>
2023-07-21 14:13:50 +0530

    Add project files.

---
+++ RestApi_Salesforce/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+namespace RestApi_Salesforce
+{
+    class Program
+    {
+        private static salesforce CreateClient()
+        {
+            return new salesforce
+            {
+                Username = ConfigurationManager.AppSettings["username"],
+                Password = ConfigurationManager.AppSettings["password"],
+                Token = ConfigurationManager.AppSettings["token"],
+                ClientId = ConfigurationManager.AppSettings["clientId"],
+                ClientSecret = ConfigurationManager.AppSettings["clientSecret"],
+            };
+        }
+             static void Main(string[] args)
+            {
+                var client=CreateClient();
+                client.login();
+                Console.WriteLine(client.Query("select name from Account"));
+            }
+        
+    }
+}
---
+++ RestApi_Salesforce/salesforce.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RestApi_Salesforce
+{
+     class salesforce
+    {
+        private const string Login_Endpoint = "https://login.salesforce.com/services/oauth2/token";
+        private const string Api_Endpoint = "/Services/data/v51.0/";
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string AuthToken { get; set; }
+        public string InstanceUrl { get; set; }
+
+        static salesforce()
+        {
+            // connectivity secured
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+        }
+        public void login()
+        {
+            String jsonResponse;
+            using (var Client = new HttpClient())
+            {
+                var request = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    {"grant_type",Password },
+                    {"client_id",ClientId },
+                    {"Client_Secret",ClientSecret }, 
+                    {"Username",Username},
+                    {"password",Password+Token }
+                });
+
+                request.Headers.Add("X-PreetyPrint", "1");
+                var response = Client.PostAsync(Login_Endpoint, request).Result;
+                jsonResponse = response.Content.ReadAsStringAsync().Result;
+            }
+
+            //Console.WriteLine($"Response:{jsonResponse}");
+            var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonResponse);
+            AuthToken = values["access_token"];
+            InstanceUrl = values["instance_url"];
+            Console.WriteLine("AuthToken = " + AuthToken);
+            Console.WriteLine("InstanceUrl = " + InstanceUrl);
+
+        } //login function ends
+        
+        public string Query(string soqlQuery)
+        {
+            using (var client = new HttpClient())
+            {
+                string restRequest = InstanceUrl + Api_Endpoint + "query?q=" + soqlQuery;
+                var request = new HttpRequestMessage(HttpMethod.Get, restRequest);
+                request.Headers.Add("Authorization", "Bearer " + AuthToken);
+                request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Add("X-PreetyPrint", "1");
+
+                var response = client.SendAsync(request).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+
+
+    }
+}
